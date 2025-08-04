cf8adfcab0e9d0255345811511b9f496c59dcc15
ankit7078 <av8537742@gmail.com>
2025-08-04 17:07:25 +0530

    update code

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,5 @@ app.use(express.json());
 app.use("/api/reviews", require("./routes/reviewRoutes"));
 app.use("/api/contact", require("./routes/contactRoutes")); // ✅ new line
 
-
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
